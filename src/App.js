8ded22c43f7eac7953eb5c21e9a5baf30cf2222b
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-25 21:31:54 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-import './App.css'
+
+
+
 
 class App extends Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api?results=10")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          data: data.results
+        })
+      )
+  }
+
   render() {
     return (
       <div className="App">
-  
+        {this.state.data
+          ? this.state.data.map
+            ? this.state.data.map(user => (
+              <div>
+                <img src={user.picture.medium} alt={""} />
+                <div> {user.name.first + " " + user.name.last}</div>
+                <a href={user.email}> {user.email}</a>
+                <hr />
+              </div>
+            ))
+            : "Ups! It is not your day"
+          : "Be patient..."}
+
       </div>
     )
   }
